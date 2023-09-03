eb29e8f78b6c58e608c1b82fedd5c21a55ac6352
SunSeekerX <1647800606@qq.com>
2023-09-03 23:59:45 +0800

    u

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,10 @@
 import { defineConfig } from 'vite'
 import uni from '@dcloudio/vite-plugin-uni'
-// import h5ProdEffectPlugin from 'uni-vite-plugin-h5-prod-effect'
 import AutoImport from 'unplugin-auto-import/vite'
 
 export default defineConfig({
   plugins: [
     uni(),
-    // 对h5 production环境打包时的特殊处理，否则uni-crazy-router在这个环境会异常
-    // h5ProdEffectPlugin(),
     AutoImport({
       include: [/\.[tj]sx?$/, /\.vue$/, /\.vue\?vue/, /\.md$/],
       imports: ['vue', 'vue-router'],
@@ -15,7 +12,6 @@ export default defineConfig({
       dirs: [],
       dts: './src/auto-imports.d.ts',
       vueTemplate: false,
-      // resolvers: [ElementPlusResolver()],
       eslintrc: {
         enabled: true,
         filepath: './.eslintrc-auto-import.json',
